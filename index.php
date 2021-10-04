004b4506cbd6220b952144cc8752d1b78c9d2685
Maria dos Anjos <80406736+mariadosanjos@users.noreply.github.com>
2021-10-04 17:42:29 -0300

    Site simples sobre metodologias ágeis

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Aprendendo básico de HTML e CSS</title>
+</head>
+
+<body class="tela">
+    <header class="container">
+        <div class="tema">
+            <h1>Treinamento ConstruDelas</h1>
+            <h2>Metodologias Ágeis</h2>
+        </div>
+        <div>
+            <img id="img-header" src="/img/metodologias-ageis.png" alt="Metodologias Ágeis">
+        </div>
+    </header>
+
+    <main >
+        <div>
+            <p class="conteudo-tema">
+                Manifesto Ágil: Valores e Princípios<br>
+            </p>
+            <p class="conteudo-manifesto">
+                O manifesto ágil é caracterizado como uma declaração valores e princípios que são essenciais para o desenvolvimento de software.
+            </p>
+            <div class="container">
+                <spam id="valores">
+                    <h3>Valores</h3>
+                    <img src="/img/valores.png" alt="Valores do Manifesto Ágil">
+                </spam>
+                <spam id="principios">
+                    <h3>Princípios</h3>
+                    <img src="/img/principios.png" alt="Princípios do Manisfesto Ágil">
+                </spam>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <section class="footer-content">
+            <section class="address">
+                <h1>QUEM SOU?</h1>
+                <p>Me chamo Maria dos Anjos, sou uma desenvolvedora web em formação que busca atuar em projetos que sejam orientados a resultados e que me mantenha imersa em desafios para que possa demonstrar meus conhecimentos.</p>
+            </section>
+            <section class="social">
+                <h1>REDES SOCIAIS</h1>
+                <ul>
+                    <li>
+                        <a href="https://github.com/mariadosanjos" target="_blank"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/maria-dos-anjos-serra-santos-143b9b168" target="_blank"><i
+                                class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+            </section>
+            <section class="about">
+                <h1>SOBRE A ACADEMIA</h1>
+                <p>Treinamento realizado pela Gama Academy em parceria com a Juntos Somos +</p>
+            </section>
+        </section>
+        <section class="copyright">
+            <span>Academia ConstruDelas | Feito com HTML <i class="fa fa-heart" aria-hidden="true"></i> CSS</span>
+        </section>
+    </footer>
+</body>
+
+</html>
